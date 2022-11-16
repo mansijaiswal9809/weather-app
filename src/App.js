1011e5eaf5e45cf459714b04827df8fdd2f49942
mansijaiwal9809 <mansijaiswal1235@gmail.com>
2022-11-17 00:00:54 +0530

    responsive done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import { useContext } from 'react';
-import { weatherContext } from './components/Context/AppProvider';
 import './App.css';
 import Bottom from './components/bottom/Bottom';
 import Temp from './components/temp/Temp';
 import Top from './components/Top/Top';
 
 function App() {
-  const {data}=useContext(weatherContext);
-  let type
-  if(data.weather){
-    type=data.weather[0].main
-    console.log(type)
-  }
   return (
-    <div style={{backgroundImage: "url('')",backgroundRepeat:'no-repeat', backgroundSize:'cover' }}>
+    <div>
     <Top/>
     <Temp/>
     <Bottom/>
--- src/components/Context/AppProvider.js
+++ src/components/Context/AppProvider.js
@@ -3,15 +3,18 @@ import { useEffect, useState, useReducer } from "react";
 import { createContext } from "react";
 import reducer from "../Reducer/reducer";
 
+
 export const weatherContext = createContext();
 const AppProvider = ({ children }) => {
 const [data, setdata] = useState({});
-  const [state, dispatch] = useReducer(reducer, "pune");
+  const [state, dispatch] = useReducer(reducer, "mumbai");
   // console.log(state)
+  const key=process.env.REACT_APP_API_KEY
+  console.log(key)
     
     useEffect(() => {
       const getWeatherDetails= async()=>{
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${state}&units=metric&appid=aeedcb522414aca47623508707283d6d`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${state}&units=metric&appid=${key}`;
         const res= await fetch(url)
         const resjson= await res.json();
         // console.log(resjson)
